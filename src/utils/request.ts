47203110cdddbefc98843c1c387b78fe1a57b593
pengwei <pengwei@antiy.cn>
2019-08-28 14:29:16 +0800

    fix request关于method的判断错误

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -12,7 +12,7 @@ interface Arg {
 
 const request = async (arg: Arg) => {
   let url = arg.url
-  let needQs = arg.method === 'get' || 'delete'
+  let needQs = ['get', 'delete'].includes(arg.method)
   url += needQs && arg.data ? '?' + qs.stringify(arg.data) : ''
   let obj: Arg = {
     method: arg.method,
